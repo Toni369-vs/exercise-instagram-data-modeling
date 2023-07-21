c6df45933e364e217dc8c545d4b239aee94267bd
Antonio Centenera Mayoral <125910370+Toni369-vs@users.noreply.github.com>
2023-07-21 09:56:09 +0000

    Tabla finalizada

--- src/models.py
+++ src/models.py
@@ -1,29 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Enum 
+from sqlalchemy.orm import relationship, declarative_base, backref
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class MediaType(enum.Enum):
+    IMAGE = 'image'
+    VIDEO = 'video'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+followers = Table('followers',
+    Base.metadata,         
+    Column('user_from_id', Integer, ForeignKey('user.userID'), primary_key=True),
+    Column('user_ID', Integer, ForeignKey('user.userID'), primary_key=True)
+)
+
+
+class User(Base):
+    __tablename__= 'user'
+    userID = Column (Integer, primary_key = True)
+    username = Column (String(50), nullable = False)
+    firstname = Column (String(50), nullable = False)
+    lastname = Column (String(50), nullable = False)
+    email = Column (String(50), nullable = False, unique = True)
+    followers = relationship ("Follower", secondary=followers, lazy="subquery", backref=backref("user", lazy=True))
+
+class Follower(Base):
+    __tablename__= 'follower'
+    id = Column(Integer, primary_key = True )
+
+
+class Post(Base):
+    __tablename__= 'post'
+    postID = Column (Integer, primary_key = True, nullable=False)
+    user_ID = Column (Integer, ForeignKey('user.userID'), nullable=False)
+
+
+class Media(Base):
+    __tablename__= 'media'
+    mediaID = Column (Integer, primary_key = True)
+    type = Column(Enum(MediaType), nullable=False)  
+    url = Column (String(250), nullable=False)
+    post_ID = Column (Integer, ForeignKey('post.postID'), nullable=False)   
+
+
+class comments(Base):
+    __tablename__= 'comments'
+    commentID = Column (Integer, primary_key = True)
+    author_ID = Column (Integer, ForeignKey('user.userID'))
+    post_ID = Column (Integer, ForeignKey('post.postID'))
 
     def to_dict(self):
         return {}
